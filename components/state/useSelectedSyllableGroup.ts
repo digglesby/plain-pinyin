6ff7f7a796e8a0316342bcb45371b9fa2fe57152
Curtis Ward <thedude@digglesby.com>
2023-08-07 07:09:10 -0400

    Fix modal scroll to top bug

--- components/state/useSelectedSyllableGroup.ts
+++ components/state/useSelectedSyllableGroup.ts
@@ -22,7 +22,15 @@ const useSyllables = (tableData: TableData): [
 
         if (!syllableGroupPinyin) {
             setSelectedSyllableGroup(undefined)
-            window.location.hash = ''
+
+            const uri = window.location.toString();
+ 
+            if (uri.indexOf("#") > 0) {
+                var clean_uri = uri.substring(0, uri.indexOf("#"));
+ 
+                window.history.replaceState({}, document.title, clean_uri);
+            }
+            
             return
         }
 
